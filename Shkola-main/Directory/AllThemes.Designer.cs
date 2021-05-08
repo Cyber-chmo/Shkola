0a36955c9cbfd37b28966f4d19a2824418e1ff65
Cyber-chmo <70958354+Cyber-chmo@users.noreply.github.com>
2021-05-08 19:52:00 +0400

    ✪ ω ✪

---
+++ Shkola-main/Directory/AllThemes.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Directory
+{
+    partial class AllThemes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AllThemes));
+            this.themeTB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tagsTB = new System.Windows.Forms.TextBox();
+            this.labelTag = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // themeTB
+            // 
+            this.themeTB.Location = new System.Drawing.Point(460, 39);
+            this.themeTB.Name = "themeTB";
+            this.themeTB.Size = new System.Drawing.Size(188, 20);
+            this.themeTB.TabIndex = 3;
+            this.themeTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.themeTB_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Candara", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(654, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 51);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tagsTB
+            // 
+            this.tagsTB.Location = new System.Drawing.Point(460, 70);
+            this.tagsTB.Name = "tagsTB";
+            this.tagsTB.Size = new System.Drawing.Size(188, 20);
+            this.tagsTB.TabIndex = 5;
+            this.tagsTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tagsTB_KeyDown);
+            // 
+            // labelTag
+            // 
+            this.labelTag.AutoSize = true;
+            this.labelTag.Font = new System.Drawing.Font("Candara Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTag.Location = new System.Drawing.Point(398, 75);
+            this.labelTag.Name = "labelTag";
+            this.labelTag.Size = new System.Drawing.Size(56, 15);
+            this.labelTag.TabIndex = 6;
+            this.labelTag.Text = "Предмет\r\n";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Candara Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Все темы:";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(13, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 345);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(397, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Предпросмотр";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(420, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Тема";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(378, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(344, 317);
+            this.panel2.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(13, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 47);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Отправить список избранного на почту\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // AllThemes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(736, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTag);
+            this.Controls.Add(this.tagsTB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.themeTB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "AllThemes";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox themeTB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tagsTB;
+        private System.Windows.Forms.Label labelTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Shkola-main/Directory/PhysicsForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Directory
+{
+    public partial class PhysicsForm : Form
+    {
+        public PhysicsForm(string predmet)
+        {
+            InitializeComponent();
+
+            Text = predmet;
+            label1.Text = predmet;
+            label3.Text = File.ReadAllText("../../../Files/" + predmet + ".txt");
+
+            if (predmet == "Физика")
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Введение");
+                comboBox1.Items.Add("Плотность");
+                comboBox1.Items.Add("Сила тяжести");
+                comboBox1.Items.Add("Архимедова сила");
+
+
+                //label3.Text = File.ReadAllText("../../../Files/Физика.txt"); 
+
+                // pictureBox1.Load("../../../Pictures/Physics1.jpg");
+                //  pictureBox2.Load("../../../Pictures/Physics2.jpg");
+            }
+            else if (predmet == "Русский язык")
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Введение");
+                comboBox1.Items.Add("Причастие");
+                comboBox1.Items.Add("Деепричастие");
+            }
+            else if (predmet == "Алгебра")
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Введение");
+                comboBox1.Items.Add("Одночлены и Многочлены");
+                comboBox1.Items.Add("Квадратные уравнения");
+                comboBox1.Items.Add("Биквадратные уравнения");
+                comboBox1.Items.Add("Неполные квадратные уравнения");
+
+            }
+            else if (predmet == "Геометрия")
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Введение");
+                comboBox1.Items.Add("Прямая. Углы.");
+                comboBox1.Items.Add("Нет");
+                comboBox1.Items.Add("Ещё что-то");
+            }
+            else if (predmet == "Химия")
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Введение");
+                comboBox1.Items.Add("О");
+                comboBox1.Items.Add("О2");
+                comboBox1.Items.Add("О3");
+            }
+
+
+
+
+
+        }
+
+        private void PhysicsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            { 
+                try
+                {
+                    label3.Text = File.ReadAllText("../../../Files/Темы/" + label1.Text + " " + comboBox1.Text + ".txt");
+                }
+                catch (Exception) 
+                {
+                    MessageBox.Show("Такой темы нет");
+                }
+            }
+        }
+    }
+}
